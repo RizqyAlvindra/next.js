81d7aa45426d86356b1843061a2df4c7e0340479
Tomsykesss <90166916+RizqyAlvindra@users.noreply.github.com>
2022-08-23 16:09:03 +0700

    hgcffg

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
-
-module.exports = nextConfig

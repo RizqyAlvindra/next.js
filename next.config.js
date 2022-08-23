292c2177f221d2f6ee1a86e91b7cf1d00f588761
Tomsykesss <90166916+RizqyAlvindra@users.noreply.github.com>
2022-08-23 16:12:41 +0700

    asdasd

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
+  exportPathMap: function() {
+    return {
+      "/": { page:"/" }
+    };
+  }
 }
